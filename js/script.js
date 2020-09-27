236ea9f827957552475327b00680bbc9ad7006ea
Рустам <rooxsystem@gmail.com>
2020-09-27 12:18:32 +0300

    first commit

--- js/script.js
+++ js/script.js
@@ -1,1 +1,22 @@
 "use strict";
+/*
+function clientName() {
+    let age = prompt('Сколько тебе лет?');
+    alert(`Возраст подходит. Вам ${age} лет!`);
+
+    let name = prompt('Как тебя зовут?');
+    alert(`Добро пожаловать, ${name}!`);
+
+    confirm(`Готов начать, ${name}?`);
+}
+
+clientName();
+*/
+
+container_button.onclick = function() {
+    let name = prompt('Введите ваше имя');
+    let orderAdress = prompt('Введите адрес доставки');
+    let dateOrder = prompt('Введите дату доставки');
+    alert(`${name}, Вы подтверждаете Ваш заказ?`);
+    alert(`${name}, Ваш заказ оформлен!\nДоставка по адресу: ${orderAdress}\nДата доставки: ${dateOrder}\nСпасибо за Ваш заказ!`);
+}
